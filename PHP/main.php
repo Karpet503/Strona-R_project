898cc840174b498032f6faf2c0e1dcc5a9bca8aa
TLLogika <skonrad203@interia.pl>
2026-03-26 00:54:32 +0100

    adding php and server conectivity (later)

--- PHP/main.php
+++ PHP/main.php
@@ -1,1 +1,15 @@
-test
+<?php
+$servername = "100.113.36.12:3307";
+$username = "project";
+$password = "paw0rd123";
+$dbname = "BazaTestowa";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
