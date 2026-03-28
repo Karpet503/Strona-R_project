d4ee39ff63f9d658dc96ff88880506fb4d854f61
Karpet503 <bakunkarol@gmail.com>
2026-03-28 16:59:25 +0100

    dodanie side baru który się otwiera izamyka przy kliknięciu login i X

--- JS/logowanie.js
+++ JS/logowanie.js
@@ -1,12 +1,27 @@
-/*
-function bluruj(){
-    const overlay = document.querySelector('.overlay');
-    overlay.classList.toggle('active');
-}
-    
-*/
 
 
-function bluruj() {
+/*function bluruj() {
   document.querySelector('.overlay').style.display = "block";
+
+}*/
+
+
+
+const menuBtnopen = document.querySelector('.logowanie',);
+if (menuBtnopen) { 
+    menuBtnopen.addEventListener('click', () => {
+      
+        document.querySelector('body').classList.toggle('closed');
+        document.querySelector('.overlay').style.display = "block";
+    })
+}
+
+const menuBtnclose = document.querySelector('.zamknij',);
+if (menuBtnclose) { 
+    menuBtnclose.addEventListener('click', () => {
+      
+        document.querySelector('body').classList.toggle('closed');
+        document.querySelector('.overlay').style.display = "none";
+        
+    })
 }
