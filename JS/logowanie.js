5635834918bb158cac0aedf4c1bcc2685d743666
Karpet503 <bakunkarol@gmail.com>
2026-03-28 01:48:55 +0100

    blur na guzik logowanie(przygotowanie pod formularz logowania)

---
+++ JS/logowanie.js
@@ -0,0 +1,12 @@
+/*
+function bluruj(){
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.toggle('active');
+}
+    
+*/
+
+
+function bluruj() {
+  document.querySelector('.overlay').style.display = "block";
+}
